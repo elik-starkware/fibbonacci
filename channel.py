c0c1605ad28ef46182529b242eac45d823aa57ff
Arad Reder <arad@starkware.co>
2025-03-12 16:08:25 +0200

    wip verifier

--- channel.py
+++ channel.py
@@ -4,32 +4,52 @@ from hash import my_hash
 
 
 class Channel:
-    def __init__(self, F):
+    def __init__(self, F, N):
         """Initialize the channel with an optional random seed."""
-        self.messages = Queue()
+        self.messages = []
+        self.decommit_idx = None
         self.F = F
+        self.N = N
+
+    def start_decommit(self):
+        assert not self.decommit_idx, "Decommitment already started"
+        assert len(self.messages) > 0
+        self.decommit_idx = len(self.messages)
 
     def send(self, message: dict[str, int | str]):
         """Send (store) a message in the channel."""
-        self.messages.put(message)
+        self.messages.append(message)
 
-    def receive(self):
+    def receive_commitment(self):
         """Receive (retrieve) the last message sent."""
-        return self.messages.get()
+        return self.messages[:self.decommit_idx] if self.decommit_idx else self.messages
+
+    def receive_decommitment(self):
+        """Receive (retrieve) the last message sent."""
+        assert self.decommit_idx, "Decommitment not started"
+        return self.messages[self.decommit_idx:]
 
     def get_all_messages(self):
         """Retrieve all messages exchanged."""
-        return list(self.messages.queue)
+        return self.messages
 
     def _get_hash(self):
-        return my_hash("".join(map(lambda x: str(dumps(x)), self.messages.queue)))
+        return my_hash("".join(map(lambda x: str(dumps(x)), self.messages)))
 
+    def receive_random_query(self, title: str = "Random Query"):
+        idx = int(self._get_hash(), 16) % self.N
+        self.messages.append({
+            "title": title,
+            "data": idx
+        })
+        return idx
+    
     def receive_random_field_element(self, title: str = "Random Field Element"):
         # TODO change with nonce
         """Generate a random f"""
         random_field_element = self.F(int(self._get_hash(), 16))
-        self.messages.put({
+        self.messages.append({
             "title": title,
-            "data": str(random_field_element)
+            "data": random_field_element
         })
         return random_field_element
