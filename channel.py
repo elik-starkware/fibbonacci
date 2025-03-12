f58641263f9b4be9929086f3246230b536893fa5
Arad Reder <arad@starkware.co>
2025-03-12 13:52:15 +0200

    pushing to main like a boss

--- channel.py
+++ channel.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from hashlib import sha256
+from pickle import dumps
 
 
 class Channel:
@@ -21,7 +22,7 @@ class Channel:
         return list(self.messages.queue)
 
     def _get_hash(self):
-        return sha256("".join(map(lambda x: x["data"], self.messages.queue)).encode()).hexdigest()
+        return sha256("".join(map(lambda x: str(dumps(x)), self.messages.queue)).encode()).hexdigest()
 
     def receive_random_field_element(self, title: str = "Random Field Element"):
         # TODO change with nonce
